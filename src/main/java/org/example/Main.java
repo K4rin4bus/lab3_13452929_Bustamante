414dcb5349d654e3cf7ef3004e81bed45fc19c94
Karina Bustamante <karina.bustamante@usach.cl>
2023-12-03 22:39:51 -0300

    Avance Requerimientos funcionales, creacion proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+ package org.example;
+
+//Laboratorio 3 Paradigmas de la programación
+
+ public class Main {
+     public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
